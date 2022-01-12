3c73aadd0fbaba84ccc47bacbb4fb63c3606a596
Ayush Chakravarthy <ayush.k.chakravarthy@gmail.com>
2022-01-11 21:54:39 -0800

    removed seeding

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import torch
 import numpy as np
 import time 
 
-random_seed = 3417
-torch.manual_seed(random_seed)
-torch.cuda.manual_seed(random_seed)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-np.random.seed(random_seed)
-
 from train import train
 
 parser = argparse.ArgumentParser()
